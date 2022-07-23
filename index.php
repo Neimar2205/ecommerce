0db5beb5cdde9198cd152f028b085173307c2b3f
Neimar <neimar.jdlg@gmail.com>
2022-07-23 20:13:39 -0300

    aula110 Site-categorias

--- index.php
+++ index.php
@@ -218,6 +218,15 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	exit;
 });
 
+$app->get("/categories/:idcategory", function($idcategory){
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$page = new Page();
+	$page->setTPL("category", ['category'=>$category->getValues(),
+								'products'=>[]
+							]);	
+});
+
 
 $app->run();
 
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -20,7 +20,8 @@ class Category extends Model {
             array(":idcategory" =>$this->getidcategory(),
                   ":descategory" =>$this->getdescategory()
         ));        
-        $this->setData($results[0]);         
+        $this->setData($results[0]);
+        Category::updateFile();
     } 
     
     public function get($idcategory){
@@ -37,6 +38,18 @@ class Category extends Model {
         $sql->query("DELETE FROM tb_categories WHERE idcategory = :idcategory",
                     array(":idcategory"=>$this->getidcategory()
                     ));
+       Category::updateFile();
+    }
+
+    public static function updateFile(){
+        $categories = Category::listAll();
+        $html = [];
+        foreach ($categories as $row) {
+            array_push($html,'<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+        //aqui é feito o carregamento do arquivo "categories-menu.html" localizado na views.
+        // Desta forma o menu é carregado dinamicamente.
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
 
 }
